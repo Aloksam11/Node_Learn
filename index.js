3e347caad8441fe786f05ae1c9f45d17f505a2a2
Alok Chauhan <chauhan.alok1995@gmail.com>
2022-12-17 18:52:10 +0530

    PUT and Validation added

--- index.js
+++ index.js
@@ -1,39 +1,49 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const Joi = require("joi");
+const { result } = require("underscore");
+const { join } = require("path");
+const app = express();
 app.use(express.json());
-const courses = [{
+const courses = [
+  {
     id: 1,
-    name : "Javascript"    
-},
-{
+    name: "javascript",
+  },
+  {
     id: 2,
-    name : "React"
-},
-{
+    name: "react",
+  },
+  {
     id: 3,
-    name : "FSD"
-}];
+    name: "FSD",
+  },
+];
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
-app.get("/courses", function(req,res){
-    res.send(courses);
-})
-app.get("/courses/:id", function(req,res){
-    let courseId = req.params.id;
-    // res.send(courseId);
-    let a = courses.find(ele => ele.id === parseInt(courseId));
-    if(!a){
-        res.status(404).send("course not found");
-    } else {
-        res.send(a);
-    }
-})
+app.get("/", function (req, res) {
+  res.send("Hello World");
+});
+app.get("/courses", function (req, res) {
+  res.send(courses);
+});
+app.get("/courses/:id", function (req, res) {
+  var courseId = req.params.id;
+  //array.find
+  //array.push
+  var course = courses.find((c) => c.id === parseInt(courseId));
+  if (!course) {
+    res.status(404).send("Course not found");
+  } else res.send(course);
+});
 
-///POST
+//post servidce
 app.post("/course", function (req, res) {
-    //course object
+  //course object
+  //validation of the input
+  const validateResult = validateCourse(req.body);
+  console.log(validateResult);
+
+  if (validateResult.error) res.send(validateResult.error);
+  else {
     var course = {
       id: courses.length + 1,
       name: req.body.name,
@@ -41,7 +51,42 @@ app.post("/course", function (req, res) {
     //push that object into the courses array
     courses.push(course);
     res.send(course);
-  });  
+  }
+});
 
-app.listen(3000)
-console.log("Running the 3000");
+//PUT Code
+app.put("/courses/:id", function (req, res) {
+    const validateResult = validateCourse(req.body);
+    console.log(validateResult);
+  
+    if (validateResult.error) res.send(validateResult.error);
+    else {
+      var courseId = req.params.id;
+      var course = courses.find((c) => c.id === parseInt(courseId));
+      let courseindex = courses.findIndex((c) => c.id === parseInt(courseId));
+      courses[courseindex].name = req.body.name;
+      res.send(courses[courseindex]);
+  
+      //update the course in that index
+      //how do we update a particular object in an array
+      //
+    }
+  });
+  
+
+app.listen(3000);
+console.log("listening the server on port 3000");
+//JOI module and validate the input for post request
+function validateCourse(course) {
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+  });
+
+  try {
+    const result = schema.validate(course);
+    return result;
+  } catch (err) {
+    return err;
+  }
+}
+
